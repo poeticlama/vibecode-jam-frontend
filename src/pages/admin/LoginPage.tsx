5425be8b0d389b88460eb46039cc273720cb13ad
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-11-26 18:24:36 +0300

    fix: login page redirect after authorization

--- src/pages/admin/LoginPage.tsx
+++ src/pages/admin/LoginPage.tsx
@@ -1,8 +1,8 @@
-import { useState, type FormEvent } from 'react';
+import { useState, type FormEvent, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 
-import { useLoginMutation } from '../../store/api/endpoints/auth.api.ts';
 import { useAppSelector } from '../../hooks/rtk.ts';
+import { useLoginMutation } from '../../store/api/endpoints/auth.api.ts';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
@@ -12,26 +12,31 @@ const LoginPage = () => {
   const location = useLocation();
   const { isAuthenticated } = useAppSelector((state) => state.auth);
 
-  // Если уже авторизован, перенаправляем
-  if (isAuthenticated) {
-    const from = (location.state as { from?: Location })?.from?.pathname || '/admin';
-    navigate(from, { replace: true });
-    return null;
-  }
-
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       await login({ username, password }).unwrap();
+
       // После успешного логина перенаправляем
-      const from = (location.state as { from?: Location })?.from?.pathname || '/admin';
+      const from =
+        (location.state as { from?: Location })?.from?.pathname || '/admin';
+
       navigate(from, { replace: true });
     } catch (err) {
       // Ошибка обрабатывается через error из useLoginMutation
       console.error('Login failed:', err);
     }
   };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const from =
+        (location.state as { from?: Location })?.from?.pathname || '/admin';
+
+      navigate(from, { replace: true });
+    }
+  });
 
   return (
     <div className="mt-20 flex h-full w-full flex-col items-center gap-10">
@@ -64,7 +69,8 @@ const LoginPage = () => {
             {error && (
               <div className="mb-3 text-sm text-red-500">
                 {'data' in error
-                  ? (error.data as { message?: string })?.message || 'Ошибка входа'
+                  ? (error.data as { message?: string })?.message ||
+                    'Ошибка входа'
                   : 'Ошибка входа'}
               </div>
             )}
